8bb3f21ae1f765890b5f2b53da0df991a23c1860
Andr0id_Reptil0id <1@1.com>
2022-05-24 13:55:51 +0300

    first homework

---
+++ homework_1.1.py
@@ -0,0 +1,12 @@
+duration = 400153
+
+if duration < 60:
+    print(duration, 'секунд')
+elif duration < 3600:
+    print(f'{duration // 60} минут {duration % 60} секунд')
+elif duration < 86400:
+    print(f'{duration // 3600} часов {duration % 3600 // 60} минут {duration % 60} секунд')
+else:
+    print(f'{duration // 86400} дней {duration // 86400 // 3600} часов {duration % 3600 // 60} минут {duration % 60} секунд')
+
+# я не понял как это решать, простите
